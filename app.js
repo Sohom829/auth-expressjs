2af8901677f80ba2a4b9cfe842e6e483575e3c0e
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-03-15 23:32:51 +0600

    Update backend

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const dbURL = process.env.MONGO_URL;
 const User = require("./models/user.model");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const fs = require("fs");
+const cheerio = require("cheerio");
+const session = require("express-session");
+
+app.use(
+  session({
+    secret: `${process.env.SecretKey}`,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 mongoose
   .connect(dbURL)
@@ -22,6 +33,8 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("views"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/./views/index.html");
 });
@@ -30,6 +43,7 @@ app.post("/register", async (req, res) => {
   try {
     bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
       const newUser = new User({
+        username: req.body.username,
         email: req.body.email,
         password: hash,
       });
@@ -48,11 +62,19 @@ app.post("/login", async (req, res) => {
   try {
     const email = req.body.email;
     const password = req.body.password;
+    const username = req.body.username;
     const user = await User.findOne({ email: email });
     if (user) {
       bcrypt.compare(password, user.password, (err, result) => {
         if (result === true) {
-          res.status(200).json({ message: "User is logged in." });
+          const userData = {
+            email: user.email,
+            username: user.username,
+          };
+          req.session.userData = userData;
+          res.status(200).json({ message: "User is logged in.", username });
+        } else {
+          res.status(401).json({ message: "Incorrect password." });
         }
       });
     } else {
@@ -64,6 +86,42 @@ app.post("/login", async (req, res) => {
   } catch (error) {
     res.status(500).json(error.message);
   }
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
+
+app.get("/sig-up", (req, res) => {
+  res.sendFile(__dirname + "/views/signup.html");
+});
+
+app.get("/user", async (req, res) => {
+  const userData = req.session.userData;
+  if (userData) {
+    res.json(userData);
+  } else {
+    res.status(401).json({ message: "User not authenticated." });
+  }
+});
+
+app.get("/dash", async (req, res) => {
+  const html = fs.readFileSync("views/dash.html");
+
+  const $ = cheerio.load(html);
+
+  const fetchUser = async () => {
+    const response = await fetch("http://localhost:4000/user");
+    const data = await response.json();
+
+    // Set the user name and email on the HTML elements
+    $("h2").text(data.username);
+    $("h3").text(data.email);
+  };
+
+  await fetchUser();
+
+  res.send($.html());
 });
 
 app.use((req, res, next) => {
